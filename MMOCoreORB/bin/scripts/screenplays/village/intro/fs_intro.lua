dc04c5910d38df5fc696bb4b6e34e1e48f6ffe33
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-06-07 15:54:20 -0400

    fix: ensure consistent step delay for Sith shadow attack in fs_intro script

--- MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
@@ -13,7 +13,7 @@ FsIntro = ScreenPlay:new {
 
 	stepDelay = {
 		[1] = { 3600, 3600 }, -- Old man visit, 1 hour.
-		[3] = { 3600, 3600 } -- Sith shadow attack, 1 hour.
+		[3] = { 3600, 3600 }, -- Sith shadow attack, 1 hour.
 }
 
 function FsIntro:getCurrentStep(pPlayer)
