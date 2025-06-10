cac4eb88ae9eb7fac4f1ab04d2a940fb4b858a6b
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-06-09 22:31:16 -0400

    fix: set factory timer to 1 second per item for consistent processing

--- MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
@@ -566,7 +566,7 @@ bool FactoryObjectImplementation::startFactory() {
 	timer = 30;
 	info(true) << "Factory Testing Timer Set To: " << timer;
 #else
-	timer = ((int)schematic->getComplexity()) * 8;
+	timer = 1; //1 second per item
 #endif
 
 	if (!populateSchematicBlueprint(schematic))
