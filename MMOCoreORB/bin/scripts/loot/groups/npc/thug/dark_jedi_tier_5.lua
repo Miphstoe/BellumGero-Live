e598648030ef539ec5983e0ceec3d41d573c7715
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-07-03 18:24:29 -0400

    Adjust loot weights for various NPC tiers and enhance Jedi crystal stats

--- MMOCoreORB/bin/scripts/loot/groups/npc/thug/dark_jedi_tier_5.lua
+++ MMOCoreORB/bin/scripts/loot/groups/npc/thug/dark_jedi_tier_5.lua
@@ -5,19 +5,15 @@ dark_jedi_tier_5 = {
 	minimumLevel = 0,
 	maximumLevel = -1,
 	lootItems = {
-		{groupTemplate = "armor_attachments", weight = 1100000},
-		{groupTemplate = "clothing_attachments", weight = 1100000},
-		{groupTemplate = "dark_jedi_common", weight = 800000},
-		{groupTemplate = "holocron_dark", weight = 550000},
-		{groupTemplate = "holocron_light", weight = 500000},
-		{groupTemplate = "melee_weapons", weight = 1200000},
-		{groupTemplate = "power_crystals", weight = 500000},
-		{groupTemplate = "ranged_weapons", weight = 3600000},
-		{groupTemplate = "resource_chemical", weight = 50000},
-		{groupTemplate = "resource_gemstone", weight = 50000},
-		{groupTemplate = "resource_metal", weight = 50000},
-		{groupTemplate = "resource_water", weight = 50000},
-		{groupTemplate = "crafting_component_advanced", weight = 450000},
+		{groupTemplate = "armor_attachments", weight = 1272750},
+		{groupTemplate = "clothing_attachments", weight = 1272750},
+		{groupTemplate = "dark_jedi_common", weight = 925500},
+		{groupTemplate = "holocron_dark", weight = 636188},
+		{groupTemplate = "holocron_light", weight = 578625},
+		{groupTemplate = "melee_weapons", weight = 945000},
+		{groupTemplate = "power_crystals", weight = 1691875},
+		{groupTemplate = "ranged_weapons", weight = 1899450},
+		{groupTemplate = "crafting_component_advanced", weight = 520688},
 	}
 }
 
