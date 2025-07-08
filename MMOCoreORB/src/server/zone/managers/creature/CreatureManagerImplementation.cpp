10e0f725c0a2c99b72a241b00989439db7350ee7
Pitussauro <213523259+Pitussauro@users.noreply.github.com>
2025-07-08 01:08:42 -0300

    Adds /harvestarea command

--- MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
@@ -924,7 +924,7 @@ void CreatureManagerImplementation::harvest(Creature* creature, CreatureObject* 
 	if (!creature->canHarvestMe(player))
 		return;
 
-	if (!player->isInRange(creature, 7))
+	if (!player->isInRange(creature, 32)) //hardcoded harvest range
 		return;
 
 	ManagedReference<ResourceManager*> resourceManager = zone->getZoneServer()->getResourceManager();
