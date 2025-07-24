6328c34c531a64ba9da1bb95ead32a1ea6067b38
Frank Misemer III <misemerfg@gmail.com>
2025-07-24 15:12:29 -0500

    Increased Droid Item Storage

--- MMOCoreORB/bin/scripts/object/tangible/component/droid/item_storage_module_4.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/droid/item_storage_module_4.lua
@@ -45,15 +45,15 @@ object_tangible_component_droid_item_storage_module_4 = object_tangible_componen
 
 
 	dataObjectComponent = "DroidItemStorageModuleDataComponent",
-	numberExperimentalProperties = {1, 1, 2, 1, 2, 2},
-	experimentalProperties = {"XX", "XX", "CD", "OQ", "XX", "CD", "OQ", "CD", "OQ"},
+	numberExperimentalProperties = {1, 1, 1, 1, 1, 1},
+	experimentalProperties = {"XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX"},
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "exp_durability", "null", "exp_effectiveness", "exp_effectiveness"},
 	experimentalSubGroupTitles = {"null", "null", "decayrate", "hitpoints", "mechanism_quality", "storage_module"},
-	experimentalMin = {0, 0, 5, 1000, -10, 7},
-	experimentalMax = {0, 0, 15, 1000, 15, 9},
+	experimentalMin = {0, 0, 15, 1000, 60, 60},
+	experimentalMax = {0, 0, 15, 1000, 60, 60},
 	experimentalPrecision = {0, 0, 0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 1, 4, 1, 1},
+	experimentalCombineType = {0, 0, 1, 1, 1, 1},
 }
 
 ObjectTemplates:addTemplate(object_tangible_component_droid_item_storage_module_4, "object/tangible/component/droid/item_storage_module_4.iff")
