a6640ebe254e2e1d1841e7f748b40831fb9c7cc4
Frank Misemer III <misemerfg@gmail.com>
2025-08-01 16:32:30 -0500

    Replaced Armor and Weps with Components

--- MMOCoreORB/bin/scripts/loot/groups/npc/dathomir/mtn_clan_tier_4.lua
+++ MMOCoreORB/bin/scripts/loot/groups/npc/dathomir/mtn_clan_tier_4.lua
@@ -5,18 +5,12 @@ mtn_clan_tier_4 = {
 	minimumLevel = 0,
 	maximumLevel = -1,
 	lootItems = {
-
-		{groupTemplate = "armor_attachments", weight = 1000000},           -- 10.00%
-		{groupTemplate = "clothing_attachments", weight = 1000000},        -- 10.00%
-		{groupTemplate = "color_crystals", weight = 1200000},              -- 12.00%
-		{groupTemplate = "melee_weapons", weight = 1700000},               -- 17.00%
-		{groupTemplate = "power_crystals", weight = 1000000},              -- 10.00%
-		{groupTemplate = "ranged_weapons", weight = 1650000},              -- 16.50%
-		{groupTemplate = "tailor_components", weight = 500000},            -- 5.00%
-		{groupTemplate = "wearables_uncommon", weight = 500000},           -- 5.00%
-		{groupTemplate = "crafting_component", weight = 500000},           -- 5.00%
-		{groupTemplate = "crafting_component_advanced", weight = 1200000}  -- 12.00%
-
+		{groupTemplate = "armor_attachments", weight = 2500000},
+		{groupTemplate = "clothing_attachments", weight = 2500000},
+		{groupTemplate = "power_crystals", weight = 2000000},
+		{groupTemplate = "weapon_component", weight = 500000},		
+		{groupTemplate = "weapon_component_advanced", weight = 2000000},
+		{groupTemplate = "color_crystals", weight = 500000},
 	}
 }
 
