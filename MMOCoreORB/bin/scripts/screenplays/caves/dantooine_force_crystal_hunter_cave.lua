da9ee11c6193de51be818db46ac15eca18664a2b
Frank Misemer III <misemerfg@gmail.com>
2025-08-07 15:45:16 -0500

    Made FRS awarded in FCC

--- MMOCoreORB/bin/scripts/screenplays/caves/dantooine_force_crystal_hunter_cave.lua
+++ MMOCoreORB/bin/scripts/screenplays/caves/dantooine_force_crystal_hunter_cave.lua
@@ -1,56 +1,85 @@
+----------------------------------------
+--  ForceCrystalCaveScreenPlay
+--  Rewards Force-Rank XP on cave mob kills
+----------------------------------------
 ForceCrystalCaveScreenPlay = ScreenPlay:new {
-	numberOfActs = 1,
+    numberOfActs       = 1,
+    screenplayName     = "ForceCrystalCaveScreenPlay",
 
-	screenplayName = "ForceCrystalCaveScreenPlay",
-
-	lootContainers = {
-		200335,
-		200336,		
-		8535511			
-	},
-	
-	lootLevel = 36,	
-
-	lootGroups = {
-		{
-			groups = {
-				{group = "color_crystals", chance = 3500000},
-				{group = "junk", chance = 3500000},
-				{group = "rifles", chance = 1000000},
-				{group = "pistols", chance = 1000000},
-				{group = "clothing_attachments", chance = 500000},
-				{group = "armor_attachments", chance = 500000}
-			},
-			lootChance = 8000000
-		}					
-	},
-	
-	lootContainerRespawn = 1800 -- 30 minutes
+    lootContainers     = { 200335, 200336, 8535511 },
+    lootLevel          = 36,
+    lootGroups         = {
+        {
+            groups = {
+                { group = "color_crystals",       chance = 3500000 },
+                { group = "junk",                 chance = 3500000 },
+                { group = "rifles",               chance = 1000000 },
+                { group = "pistols",              chance = 1000000 },
+                { group = "clothing_attachments", chance =  500000 },
+                { group = "armor_attachments",    chance =  500000 }
+            },
+            lootChance = 8000000
+        }
+    },
+    lootContainerRespawn = 1800  -- 30 minutes
 }
 
 registerScreenPlay("ForceCrystalCaveScreenPlay", true)
 
 function ForceCrystalCaveScreenPlay:start()
-	if (isZoneEnabled("dantooine")) then
-		self:spawnMobiles()
-		self:initializeLootContainers()
-	end
+    if isZoneEnabled("dantooine") then
+        self:spawnMobiles()
+        self:initializeLootContainers()
+    end
 end
 
-function ForceCrystalCaveScreenPlay:spawnMobiles()   
-	spawnMobile("dantooine", "force_crystal_hunter",1800,89,-62,-13.4,-139,8535485)
-	spawnMobile("dantooine", "force_crystal_hunter",1800,52.5,-67.9,-42.9,32,8535484)
-	spawnMobile("dantooine", "force_crystal_hunter",1800,76.3,-77,-89.3,-81,8535486)
-	
-	spawnMobile("dantooine", "force_sensitive_crypt_crawler",1800,26.1,-43,-68.3,84,8535484)
-	spawnMobile("dantooine", "force_sensitive_crypt_crawler",1800,64.1,-68.9,-36.8,86,8535485)
-	spawnMobile("dantooine", "force_sensitive_crypt_crawler",1800,85.3,-77.2,-62.9,-57,8535486)
-	spawnMobile("dantooine", "force_sensitive_crypt_crawler",1800,69.3,-75.7,-65.4,30,8535486)
-	
-	spawnMobile("dantooine", "untrained_wielder_of_the_dark_side",1800,0.7,-13.6,-6.9,-82,8535483)
-	spawnMobile("dantooine", "untrained_wielder_of_the_dark_side",1800,65.6,-77,-78.4,-10,8535486)
-	spawnMobile("dantooine", "untrained_wielder_of_the_dark_side",1800,23.8,-38.4,-32.8,-2,8535484)
-	spawnMobile("dantooine", "untrained_wielder_of_the_dark_side",1800,22.4,-42.1,-64.1,38,8535484)
-	spawnMobile("dantooine", "untrained_wielder_of_the_dark_side",1800,49.8,-48.5,-65.6,-51,8535484)
-	spawnMobile("dantooine", "untrained_wielder_of_the_dark_side",1800,49.7,-48,-17.7,167,8535484)
+function ForceCrystalCaveScreenPlay:spawnMobiles()
+    local spawnPoints = {
+        { "force_crystal_hunter",           89,   -62,   -13.4, -139, 8535485 },
+        { "force_crystal_hunter",           52.5, -67.9, -42.9,   32, 8535484 },
+        { "force_crystal_hunter",           76.3, -77,   -89.3,  -81, 8535486 },
+        { "force_sensitive_crypt_crawler",  26.1, -43,   -68.3,   84, 8535484 },
+        { "force_sensitive_crypt_crawler",  64.1, -68.9, -36.8,   86, 8535485 },
+        { "force_sensitive_crypt_crawler",  85.3, -77.2, -62.9,  -57, 8535486 },
+        { "force_sensitive_crypt_crawler",  69.3, -75.7, -65.4,   30, 8535486 },
+        { "untrained_wielder_of_the_dark_side",  0.7,  -13.6,  -6.9,  -82, 8535483 },
+        { "untrained_wielder_of_the_dark_side", 65.6, -77,   -78.4,  -10, 8535486 },
+        { "untrained_wielder_of_the_dark_side", 23.8, -38.4, -32.8,   -2, 8535484 },
+        { "untrained_wielder_of_the_dark_side", 22.4, -42.1, -64.1,   38, 8535484 },
+        { "untrained_wielder_of_the_dark_side", 49.8, -48.5, -65.6,  -51, 8535484 },
+        { "untrained_wielder_of_the_dark_side", 49.7, -48,   -17.7,  167, 8535484 }
+    }
+
+    for i, data in ipairs(spawnPoints) do
+        local tpl     = data[1]
+        local x, y, z = data[2], data[3], data[4]
+        local heading = data[5]
+        local cell    = data[6]
+
+        local pMob = spawnMobile("dantooine", tpl, 1800, x, y, z, heading, cell)
+        if pMob then
+            Logger:log(
+              string.format("ForceCrystalCaveScreenPlay: spawned '%s' #%d at [%.1f,%.1f,%.1f] cell %d",
+                            tpl, i, x, y, z, cell),
+              LT_INFO
+            )
+            createObserver(OBJECTDESTRUCTION,
+                           "ForceCrystalCaveScreenPlay",
+                           "onCaveMobDied",
+                           pMob)
+        else
+            Logger:log(
+              string.format("ForceCrystalCaveScreenPlay: FAILED to spawn '%s' #%d", tpl, i),
+              LT_ERROR
+            )
+        end
+    end
+end
+
+function ForceCrystalCaveScreenPlay:onCaveMobDied(pMob, pKiller)
+    if pKiller and SceneObject(pKiller):isPlayerCreature() then
+        -- award 100 Force-Rank XP
+        CreatureObject(pKiller):awardExperience("force_rank_xp", 100, true)
+    end
+    return 1
 end
