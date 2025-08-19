220b615d2aebad544db301d940397bdce7011ce2
Frank Misemer III <misemerfg@gmail.com>
2025-08-19 15:01:34 -0500

    Added Black Sun missions and Giant Peko Pekos

--- MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/global/global_black_sun_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/global/global_black_sun_neutral_none.lua
@@ -1,17 +1,18 @@
 global_black_sun_neutral_none = Lair:new {
 	mobiles = {
-		{"black_sun_assassin",1},
-		{"black_sun_guard",2},
-		{"black_sun_henchman",2},
-		{"black_sun_thug",3}
+		{"black_sun_assassin",2},
+		{"black_sun_guard",4},
+		{"black_sun_henchman",4},
+		{"black_sun_thug",6}
 	},
-	spawnLimit = 9,
+	spawnLimit = 18,
 	buildingsVeryEasy = {},
 	buildingsEasy = {},
 	buildingsMedium = {},
 	buildingsHard = {},
 	buildingsVeryHard = {},
 	mobType = "npc",
+	missionBuilding = "object/tangible/lair/base/objective_banner_generic_2.iff",
 	buildingType = "none"
 }
 
