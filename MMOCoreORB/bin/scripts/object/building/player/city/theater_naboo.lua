7787fa62659c0d721db70d1f66089791b4f1f1a9
Miphstoe <nyre74@gmail.com>
2025-08-27 18:25:01 -0400

    Increase private_buff_mind skill modifier to 125 for theaters in Corellia, Naboo, and Tatooine

--- MMOCoreORB/bin/scripts/object/building/player/city/theater_naboo.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/theater_naboo.lua
@@ -52,7 +52,7 @@ object_building_player_city_theater_naboo = object_building_player_city_shared_t
 	planetMapCategory = "theater",
 	abilityRequired = "place_theater",
 	skillMods = {
-		{"private_buff_mind", 100},
+		{"private_buff_mind", 125},
 		{"private_med_battle_fatigue", 5},
 		{"private_safe_logout", 1}
 	},
