3fc935fbf3472b1498eb849d41bface4b8b282ae
Frank Misemer III <misemerfg@gmail.com>
2025-09-17 15:15:36 -0500

    Reworking Rancor Cave to be NS Cave

---
+++ MMOCoreORB/bin/scripts/loot/groups/npc/dathomir/nightsister_cave.lua
@@ -0,0 +1,18 @@
+nightsister_cave = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{groupTemplate = "armor_attachments", weight = 1750000},
+		{groupTemplate = "clothing_attachments", weight = 1750000},
+		{groupTemplate = "nightsister_common", weight = 1000000},
+		{groupTemplate = "power_crystals", weight = 1000000},
+        {groupTemplate = "color_crystals", weight = 1000000},
+		{groupTemplate = "weapon_component_advanced", weight = 1000000},
+		{groupTemplate = "nightsister_clothing_schematics", weight = 1000000},
+        {groupTemplate = "clonetrooper_armor_schematics", weight = 1000000},
+		{itemTemplate = "nightsister_vibro_unit", weight = 500000},
+	}
+}
+
+addLootGroupTemplate("nightsister_cave", nightsister_cave)
---
+++ MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_shaman.lua
@@ -0,0 +1,72 @@
+nightsister_shaman = Creature:new {
+	objectName = "@mob/creature_names:nightsister_shaman",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	mobType = MOB_NPC,
+	socialGroup = "nightsister",
+	faction = "",
+	level = 300,
+	chanceHit = 27.25,
+	damageMin = 1800,
+	damageMax = 3310,
+	baseXp = 27849,
+	baseHAM = 321000,
+	baseHAMmax = 392000,
+	armor = 3,
+	resists = {90,90,90,90,90,90,90,90,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+    scale = 1.30,
+
+	templates = { "object/mobile/dressed_dathomir_nightsister_elder.iff" },
+	lootGroups = {
+	{
+        groups = {
+			{group = "nightsister_cave", chance = 10000000}
+		},
+		lootChance = 10000000, -- 100.00% total chance
+	},
+	{
+        groups = {
+			{group = "nightsister_cave", chance = 10000000}
+		},
+		lootChance = 3000000, -- 30.00% total chance
+	},
+	{
+        groups = {
+			{group = "nightsister_cave", chance = 10000000}
+		},
+		lootChance = 1000000, -- 10.00% total chance
+	},
+	{
+        groups = {
+			{group = "nightsister_cave", chance = 10000000}
+		},
+		lootChance = 500000, -- 5.00% total chance
+	},
+},
+
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+	primaryWeapon = "force_sword",
+	secondaryWeapon = "force_sword_ranged",
+	conversationTemplate = "",
+
+	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+	primaryAttacks = merge(tkamaster,swordsmanmaster,fencermaster,pikemanmaster,brawlermaster,forcepowermaster),
+	secondaryAttacks = forcepowermaster
+}
+
+CreatureTemplates:addCreatureTemplate(nightsister_shaman, "nightsister_shaman")
