e1ab8fc6a2eaefa1cb8787fcd10c0001d1506f4f
Miphstoe <nyre74@gmail.com>
2025-09-25 09:00:58 -0400

    Refactor event loot items and enhance InviteCommand for area invites

--- MMOCoreORB/bin/scripts/loot/groups/events/event.lua
+++ MMOCoreORB/bin/scripts/loot/groups/events/event.lua
@@ -4,29 +4,46 @@ event = {
   minimumLevel = 0,
   maximumLevel = 0,
   lootItems = {
-    { itemTemplate = "attachment_armor",                weight = 434783 },
-    { itemTemplate = "potted_plants_sml_s02_schematic", weight = 434783 },
-    { itemTemplate = "sea_removal_tool",                weight = 434783 },
-    { itemTemplate = "bestine_quest_imp_banner",        weight = 434783 },
-    { itemTemplate = "attachment_clothing",             weight = 434783 },
-    { itemTemplate = "be_poster",                       weight = 434783 },
-    { itemTemplate = "defensive_stance_poster",         weight = 434783 },
-    { itemTemplate = "freedom_painting",                weight = 434783 },
-    { itemTemplate = "painting_bw_stormtrooper",        weight = 434783 },
-    { itemTemplate = "painting_fighter_pilot_human_01", weight = 434783 },
-    { itemTemplate = "painting_han_wanted",             weight = 434783 },
-    { itemTemplate = "painting_leia_wanted",            weight = 434783 },
-    { itemTemplate = "painting_luke_wanted",            weight = 434783 },
-    { itemTemplate = "painting_nebula_flower",          weight = 434783 },
-    { itemTemplate = "painting_schematic_transport_ship", weight = 434782 },
-    { itemTemplate = "painting_tato_s04",                 weight = 434782 },
-    { itemTemplate = "painting_trandoshan_wanted",        weight = 434782 },
-    { itemTemplate = "painting_vader_victory",            weight = 434782 },
-    { itemTemplate = "painting_zabrak_m",                 weight = 434782 },
-    { itemTemplate = "party_poster",                      weight = 434782 },
-    { itemTemplate = "RIS_diagram",                       weight = 434782 },
-    { itemTemplate = "spitting_rawl_poster",              weight = 434782 },
-    { itemTemplate = "valley_view_painting",              weight = 434782 },
-  }
+  -- Commons (24) -> 16 @ 401,042, 8 @ 401,041
+  { itemTemplate = "attachment_armor",                weight = 401042 },
+  { itemTemplate = "attachment_clothing",             weight = 401042 },
+  { itemTemplate = "be_poster",                       weight = 401042 },
+  { itemTemplate = "defensive_stance_poster",         weight = 401042 },
+  { itemTemplate = "freedom_painting",                weight = 401042 },
+  { itemTemplate = "painting_bw_stormtrooper",        weight = 401042 },
+  { itemTemplate = "painting_fighter_pilot_human_01", weight = 401042 },
+  { itemTemplate = "painting_han_wanted",             weight = 401042 },
+  { itemTemplate = "painting_leia_wanted",            weight = 401042 },
+  { itemTemplate = "painting_luke_wanted",            weight = 401042 },
+  { itemTemplate = "painting_nebula_flower",          weight = 401042 },
+  { itemTemplate = "painting_schematic_transport_ship", weight = 401042 },
+  { itemTemplate = "painting_tato_s04",               weight = 401042 },
+  { itemTemplate = "painting_trandoshan_wanted",      weight = 401042 },
+  { itemTemplate = "painting_vader_victory",          weight = 401042 },
+  { itemTemplate = "painting_zabrak_m",               weight = 401042 },
+
+  { itemTemplate = "party_poster",                    weight = 401041 },
+  { itemTemplate = "RIS_diagram",                     weight = 401041 },
+  { itemTemplate = "spitting_rawl_poster",            weight = 401041 },
+  { itemTemplate = "valley_view_painting",            weight = 401041 },
+  { itemTemplate = "bestine_history_quest_painting",         weight = 401041 },
+  { itemTemplate = "color_crystals",                  weight = 401041 },
+  { itemTemplate = "power_crystals",                  weight = 401041 },
+  { itemTemplate = "krayt_pearls",                    weight = 401041 },
+
+  -- Rares (5) @ 50,000 each
+  { itemTemplate = "potted_plants_sml_s02_schematic", weight = 50000 },   -- RARE
+  { itemTemplate = "sea_removal_tool",                weight = 50000 },   -- RARE
+  { itemTemplate = "bestine_quest_imp_banner",        weight = 50000 },   -- RARE
+  { itemTemplate = "rebel_banner",                    weight = 50000 },   -- RARE
+  { itemTemplate = "force_color_crystal_special",     weight = 50000 },   -- RARE
+
+  -- Epics (5) @ 25,000 each
+  { itemTemplate = "clonetrooper_armor_schematics",   weight = 25000 },   -- EPIC
+  { itemTemplate = "blasterfist_schematic",           weight = 25000 },   -- EPIC
+  { itemTemplate = "bounty_hunter_armor_schematics",  weight = 25000 },   -- EPIC
+  { itemTemplate = "nightsister_clothing_schematics",    weight = 25000 },   -- EPIC
+  { itemTemplate = "bestine_quest_painting",   weight = 25000 },   -- EPIC
+}
 }
 addLootGroupTemplate("event", event)
--- MMOCoreORB/src/server/zone/objects/creature/commands/InviteCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/InviteCommand.h
@@ -1,104 +1,199 @@
-/*
-				Copyright <SWGEmu>
-		See file COPYING for copying conditions.*/
+/* Copyright <SWGEmu> See file COPYING for copying conditions.*/
 
 #ifndef INVITECOMMAND_H_
 #define INVITECOMMAND_H_
-
 
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/managers/group/GroupManager.h"
 #include "server/zone/ZoneServer.h"
+#include "server/zone/Zone.h"
 
 class InviteCommand : public QueueCommand {
 public:
+    InviteCommand(const String& name, ZoneProcessServer* server)
+        : QueueCommand(name, server) {
+    }
 
-	InviteCommand(const String& name, ZoneProcessServer* server) : QueueCommand(name, server) {
-	}
+    int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
+        if (!checkStateMask(creature))
+            return INVALIDSTATE;
 
-	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-		if (!checkStateMask(creature))
-			return INVALIDSTATE;
+        if (!checkInvalidLocomotions(creature))
+            return INVALIDLOCOMOTION;
 
-		if (!checkInvalidLocomotions(creature))
-			return INVALIDLOCOMOTION;
+        auto ghost = creature->getPlayerObject();
+        bool godMode = false;
+        
+        if (ghost != nullptr && ghost->isPrivileged()) {
+            godMode = true;
+        }
 
-		auto ghost = creature->getPlayerObject();
-		bool godMode = false;
+        auto zoneServer = server->getZoneServer();
+        if (zoneServer == nullptr)
+            return GENERALERROR;
 
-		if (ghost != nullptr && ghost->isPrivileged()) {
-			godMode = true;
-		}
+        // Check if this is an area invite command
+        StringTokenizer args(arguments.toString());
+        String firstArg;
+        
+        if (args.hasMoreTokens()) {
+            args.getStringToken(firstArg);
+            
+            // Check for "area" keyword
+            if (firstArg.toLowerCase() == "area") {
+                return doAreaInvite(creature, args, godMode, zoneServer);
+            }
+        }
 
-		auto zoneServer = server->getZoneServer();
+        // Original single player invite logic
+        auto object = zoneServer->getObject(target);
 
-		if (zoneServer == nullptr)
-			return GENERALERROR;
+        // if they didn't target a player/ship, always try lookup by typed name
+        if (object == nullptr || (!object->isPlayerCreature() && !object->isShipObject())) {
+            if (zoneServer == nullptr)
+                return GENERALERROR;
 
-		auto object = zoneServer->getObject(target);
+            auto playerMan = zoneServer->getPlayerManager();
+            if (playerMan == nullptr)
+                return GENERALERROR;
 
-/*		bool galaxyWide = ConfigManager::instance()->getBool("Core3.PlayerManager.GalaxyWideGrouping", false);
+            object = playerMan->getPlayer(firstArg);
+        }
 
-		if (galaxyWide && (object == nullptr || (!object->isPlayerCreature() && !object->isShipObject()))) 
-*/		
-   		// if they didn’t target a player/ship, always try lookup by typed name
-		if (object == nullptr || (!object->isPlayerCreature() && !object->isShipObject())){
-			StringTokenizer args(arguments.toString());
-			String firstName;
+        auto groupManager = GroupManager::instance();
+        if (object == nullptr || groupManager == nullptr)
+            return GENERALERROR;
 
-			if (args.hasMoreTokens())
-				args.getStringToken(firstName);
+        if (!object->isPlayerCreature() && !object->isShipObject()) {
+            return GENERALERROR;
+        }
 
-			if (zoneServer == nullptr)
-				return GENERALERROR;
+        CreatureObject* player = nullptr;
+        if (object->isShipObject()) {
+            auto ship = object->asShipObject();
+            if (ship != nullptr) {
+                player = ship->getOwner().get();
+            }
+        } else {
+            player = object->asCreatureObject();
+        }
 
-			auto playerMan = zoneServer->getPlayerManager();
+        if (player == nullptr)
+            return GENERALERROR;
 
-			if (playerMan == nullptr)
-				return GENERALERROR;
+        auto invitedGhost = player->getPlayerObject();
+        if (invitedGhost == nullptr)
+            return GENERALERROR;
 
-			object = playerMan->getPlayer(firstName);
-		}
+        // Cannot be invite by a player that they ignore, does not apply to privileged players
+        if (!godMode && invitedGhost->isIgnoring(creature->getFirstName()))
+            return GENERALERROR;
 
-		auto groupManager = GroupManager::instance();
+        groupManager->inviteToGroup(creature, player);
+        return SUCCESS;
+    }
 
-		if (object == nullptr || groupManager == nullptr)
-			return GENERALERROR;
+private:
+    int doAreaInvite(CreatureObject* creature, StringTokenizer& args, bool godMode, ZoneServer* zoneServer) const {
+        auto groupManager = GroupManager::instance();
+        if (groupManager == nullptr)
+            return GENERALERROR;
 
-		if (!object->isPlayerCreature() && !object->isShipObject()) {
-			return GENERALERROR;
-		}
+        // Parse range argument (default to 64 meters)
+        float range = 64.0f;
+        
+        if (args.hasMoreTokens()) {
+            String rangeStr;
+            args.getStringToken(rangeStr);
+            
+            try {
+                float parsedRange = Float::valueOf(rangeStr);
+                if (parsedRange > 0 && parsedRange <= 512.0f) { // Cap at 512 meters
+                    range = parsedRange;
+                }
+            } catch (...) {
+                // Invalid range, use default
+                creature->sendSystemMessage("Invalid range specified. Using default range of 64 meters.");
+            }
+        }
 
-		CreatureObject* player = nullptr;
+        // Get creature's zone
+        auto zone = creature->getZone();
+        if (zone == nullptr)
+            return GENERALERROR;
 
-		if (object->isShipObject()) {
-			auto ship = object->asShipObject();
+        int inviteCount = 0;
+        int maxInvites = godMode ? 50 : 20;
 
-			if (ship != nullptr) {
-				player = ship->getOwner().get();
-			}
-		} else {
-			player = object->asCreatureObject();
-		}
+        // Use a simpler approach - iterate through all players in the zone
+        auto playerManager = zoneServer->getPlayerManager();
+        if (playerManager == nullptr)
+            return GENERALERROR;
 
-		if (player == nullptr)
-			return GENERALERROR;
+        // Get all online player IDs
+        auto onlinePlayerIds = playerManager->getOnlinePlayerList();
+        
+        for (int i = 0; i < onlinePlayerIds.size() && inviteCount < maxInvites; ++i) {
+            uint64 playerId = onlinePlayerIds.get(i);
+            
+            // Get the actual player object using ZoneServer
+            auto playerObject = zoneServer->getObject(playerId);
+            if (playerObject == nullptr || !playerObject->isPlayerCreature())
+                continue;
+                
+            auto player = playerObject->asCreatureObject();
+            if (player == nullptr || player == creature)
+                continue; // Skip null or self
 
-		auto invitedGhost = player->getPlayerObject();
+            // Check if player is in the same zone
+            if (player->getZone() != zone)
+                continue;
 
-		if (invitedGhost == nullptr)
-			return GENERALERROR;
+            // Check distance
+            float distance = creature->getDistanceTo(player);
+            if (distance > range)
+                continue;
 
-		// Cannot be invite by a player that they ignore, does not apply to privileged players
-		if (!godMode && invitedGhost->isIgnoring(creature->getFirstName()))
-			return GENERALERROR;
+            auto playerGhost = player->getPlayerObject();
+            if (playerGhost == nullptr)
+                continue;
 
-		groupManager->inviteToGroup(creature, player);
+            // Skip if player is ignoring the inviter (unless godMode)
+            if (!godMode && playerGhost->isIgnoring(creature->getFirstName()))
+                continue;
 
-		return SUCCESS;
-	}
+            // Check if player is already in the same group
+            auto creatureGroup = creature->getGroup();
+            auto playerGroup = player->getGroup();
+            
+            if (creatureGroup != nullptr && playerGroup != nullptr && creatureGroup == playerGroup)
+                continue; // Already in same group
 
+            // Send the group invite
+            groupManager->inviteToGroup(creature, player);
+            inviteCount++;
+
+            // Debug message for each invite sent
+            StringBuffer inviteMsg;
+            inviteMsg << "Sent invite to: " << player->getFirstName() << " (distance: " << String::valueOf(distance) << "m)";
+            creature->sendSystemMessage(inviteMsg.toString());
+        }
+
+        // Send feedback to the inviter
+        StringBuffer msg;
+        if (inviteCount > 0) {
+            msg << "Sent group invites to " << inviteCount << " player";
+            if (inviteCount > 1) msg << "s";
+            msg << " within " << range << " meters.";
+        } else {
+            msg << "No eligible players found within " << range << " meters to invite.";
+        }
+        
+        creature->sendSystemMessage(msg.toString());
+
+        return SUCCESS;
+    }
 };
 
 #endif //INVITECOMMAND_H_
