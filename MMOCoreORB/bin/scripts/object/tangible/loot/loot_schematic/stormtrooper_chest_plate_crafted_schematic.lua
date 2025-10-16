c4c3ac6f96068cd53cce515f1809b58e46ddf952
Frank Misemer III <misemerfg@gmail.com>
2025-10-16 16:47:10 -0500

    Fix targetDraftSchematic for stormtrooper chest plate

--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/stormtrooper_chest_plate_crafted_schematic.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/stormtrooper_chest_plate_crafted_schematic.lua
@@ -4,7 +4,7 @@ object_tangible_loot_loot_schematic_stormtrooper_chest_plate_crafted_schematic =
 	objectMenuComponent = "LootSchematicMenuComponent",
 	attributeListComponent = "LootSchematicAttributeListComponent",
 	requiredSkill = "crafting_armorsmith_master",
-	targetDraftSchematic = "object/draft_schematic/clothing/clothing_armor_stormtrooper_chest.iff",
+	targetDraftSchematic = "object/draft_schematic/clothing/clothing_armor_stormtrooper_chest_plate.iff",
 	targetUseCount = 3,
 }
 
