8e20510d65f3e9fe51d4e499f12a4376764d8ca3
Frank Misemer III <misemerfg@gmail.com>
2025-10-23 16:37:50 -0500

    Updated Hit3 to be ultimate abilities

--- MMOCoreORB/bin/scripts/commands/melee2hHit3.lua
+++ MMOCoreORB/bin/scripts/commands/melee2hHit3.lua
@@ -44,13 +44,33 @@
 Melee2hHit3Command = {
         name = "melee2hhit3",
 	damageMultiplier = 4.0,
-	speedMultiplier = 2.5,
-	healthCostMultiplier = 1.25,
-	actionCostMultiplier = 2.0,
-	mindCostMultiplier = 1.25,
-        accuracyBonus = 10,
+	speedMultiplier = 3.0,
+	healthCostMultiplier = 2,
+	actionCostMultiplier = 2,
+	mindCostMultiplier = 2,
+    accuracyBonus = 25,
+	areaAction = true,
+	areaRange = 16,
 
 	stateEffects = {
+	  StateEffect( 
+		BLIND_EFFECT, 
+		{}, 
+		{ "blind_defense" }, 
+		{ "jedi_state_defense", "resistance_states" },
+		40, 
+		0, 
+		30 
+	  ),
+	  StateEffect( 
+		STUN_EFFECT, 
+		{}, 
+		{ "stun_defense" }, 
+		{ "jedi_state_defense", "resistance_states" },
+		75, 
+		0, 
+		45 
+	  ),
 	  StateEffect( 
 		DIZZY_EFFECT, 
 		{}, 
@@ -59,7 +79,7 @@ Melee2hHit3Command = {
 		50, 
 		0, 
 		30 
-	  )
+	  ),
 	},
 	
 	weaponType = TWOHANDMELEEWEAPON,
