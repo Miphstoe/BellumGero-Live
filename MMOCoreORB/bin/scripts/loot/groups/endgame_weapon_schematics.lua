c49c2456032cb1e685ff028c4390376508cc3d80
Frank Misemer III <misemerfg@gmail.com>
2025-10-27 23:37:05 -0500

    Fixed issue with lootgroup

--- MMOCoreORB/bin/scripts/loot/groups/endgame_weapon_schematics.lua
+++ MMOCoreORB/bin/scripts/loot/groups/endgame_weapon_schematics.lua
@@ -3,13 +3,13 @@ endgame_weapon_schematics = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "rifle_dc15_schematic", weight = 1000000},
-		{itemTemplate = "carbine_e5_schematic", weight = 1000000},
-		{itemTemplate = "pistol_blackfalcon_schematic", weight = 1000000},
-		{itemTemplate = "punchknuckler_schematic", weight = 1000000},
-		{itemTemplate = "lance_obsidian_schematic", weight = 1000000},
-		{itemTemplate = "2h_sword_obsidian_schematic", weight = 1000000},
-		{itemTemplate = "sword_obsidian_schematic", weight = 1000000},
+		{itemTemplate = "rifle_dc15_schematic",          weight = 1428572},
+        {itemTemplate = "carbine_e5_schematic",          weight = 1428572},
+        {itemTemplate = "pistol_blackfalcon_schematic",  weight = 1428572},
+        {itemTemplate = "punchknuckler_schematic",       weight = 1428571},
+        {itemTemplate = "lance_obsidian_schematic",      weight = 1428571},
+        {itemTemplate = "2h_sword_obsidian_schematic",   weight = 1428571},
+        {itemTemplate = "sword_obsidian_schematic",      weight = 1428571},
 	}
 }
 
