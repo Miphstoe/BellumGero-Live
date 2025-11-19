694ea1e86402f542084ee175008bf00846d4addd
Frank Misemer III <misemerfg@gmail.com>
2025-11-19 10:49:37 -0600

    Added BG Token

--- MMOCoreORB/bin/scripts/loot/groups/npc/naboo/bsv_droid_bunker.lua
+++ MMOCoreORB/bin/scripts/loot/groups/npc/naboo/bsv_droid_bunker.lua
@@ -3,13 +3,14 @@ bsv_droid_bunker = {
     minimumLevel = 0,
     maximumLevel = -1,
     lootItems = {
-        {groupTemplate = "armor_attachments",             weight = 1250000},
-        {groupTemplate = "clothing_attachments",          weight = 1250000},
+        {groupTemplate = "armor_attachments",             weight = 1200000},
+        {groupTemplate = "clothing_attachments",          weight = 1200000},
         {groupTemplate = "power_crystals",                weight = 1000000},
         {groupTemplate = "color_crystals",                weight = 1500000},
-        {groupTemplate = "weapon_component_advanced",     weight = 1500000},
+        {groupTemplate = "weapon_component_advanced",     weight = 1250000},
         {groupTemplate = "chemistry_component_advanced",  weight = 2000000},
-        {groupTemplate = "clonetrooper_armor_schematics",  weight = 1000000},
+        {groupTemplate = "clonetrooper_armor_schematics",  weight = 1100000},
+        {groupTemplate = "bg_token_group",                weight = 250000},
         {itemTemplate = "durasteel_reinforcement_core", weight = 500000},
     }
 }
