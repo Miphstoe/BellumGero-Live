3a02d1b1e193a80b2ea4e893d357aadad85c122e
Miphstoe <nyre74@gmail.com>
2026-01-02 15:59:09 -0500

    Add Dark Force Knight and Dark Force Master creatures; update spawn points in ForceCrystalCaveScreenPlay

---
+++ MMOCoreORB/bin/scripts/mobile/thug/dark_force_master.lua
@@ -0,0 +1,81 @@
+dark_force_master = Creature:new {
+	customName = "Dark Force Master",
+	mobType = MOB_NPC,
+	socialGroup = "dark_jedi",
+	faction = "",
+	level = 300,
+	chanceHit = 27.25,
+	damageMin = 1800,
+	damageMax = 3310,
+	baseXp = 27849,
+	baseHAM = 321000,
+	baseHAMmax = 392000,
+	armor = 3,
+	resists = {90,90,90,90,90,90,90,90,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+
+	templates = { "dark_jedi" },
+	lootGroups = {
+	{
+        groups = {
+			{group = "dark_jedi_tier_5", chance = 10000000}
+		},
+		lootChance = 10000000, -- 100.00% total chance
+	},
+	{
+        groups = {
+			{group = "dark_jedi_tier_5", chance = 10000000}
+		},
+		lootChance = 3000000, -- 30.00% total chance
+	},
+	{
+        groups = {
+			{group = "dark_jedi_tier_5", chance = 10000000}
+		},
+		lootChance = 1000000, -- 10.00% total chance
+	},
+	{
+        groups = {
+			{group = "blasterfist_schematic", chance = 10000000}
+		},
+		lootChance = 500000, -- 5.00% total chance
+	},
+	{
+        groups = {
+			{group = "clonetrooper_armor_schematics", chance = 10000000}
+		},
+		lootChance = 500000, -- 5.00% total chance
+	},
+		{
+			groups = {
+				{group = "bg_token_group", chance = 10000000}
+			},
+			lootChance = 250000
+		}
+},
+
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+	primaryWeapon = "dark_jedi_weapons_gen4",
+	secondaryWeapon = "dark_jedi_weapons_ranged",
+	conversationTemplate = "",
+
+	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+	primaryAttacks = merge(lightsabermaster,forcepowermaster),
+	secondaryAttacks = forcepowermaster
+}
+
+CreatureTemplates:addCreatureTemplate(dark_force_master, "dark_force_master")
