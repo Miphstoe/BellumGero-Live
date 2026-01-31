e6eb00dcfaf9bdae6987d630d0aaf881bbb4d258
Miphstoe <nyre74@gmail.com>
2026-01-31 14:47:27 -0500

    Added Safety Lock to prevent HoD from being accidently deleted  when active

--- MMOCoreORB/src/server/zone/objects/tangible/components/HolocronDestinyMenuComponent.h
+++ MMOCoreORB/src/server/zone/objects/tangible/components/HolocronDestinyMenuComponent.h
@@ -12,6 +12,9 @@
 
 class HolocronDestinyMenuComponent : public TangibleObjectMenuComponent {
 public:
+	static const uint8 RADIAL_LOCK_ITEM = 220;
+	static const uint8 RADIAL_UNLOCK_ITEM = 221;
+
 	void fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) const;
 	int handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) const;
 };
