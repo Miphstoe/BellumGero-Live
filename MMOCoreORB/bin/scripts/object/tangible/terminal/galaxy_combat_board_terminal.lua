fcd5e5434ab10f821275b869930e569a0f2a9885
Miphstoe <nyre74@gmail.com>
2026-03-19 20:34:04 -0400

    Added Galaxy combat board

---
+++ MMOCoreORB/bin/scripts/object/tangible/terminal/galaxy_combat_board_terminal.lua
@@ -0,0 +1,7 @@
+object_tangible_terminal_galaxy_combat_board_terminal = object_tangible_terminal_shared_terminal_mission_newbie:new {
+	objectMenuComponent = "GalaxyCombatBoardTerminalMenuComponent",
+	planetMapCategory = "terminal",
+	planetMapSubCategory = "terminal_mission",
+}
+
+ObjectTemplates:addTemplate(object_tangible_terminal_galaxy_combat_board_terminal, "object/tangible/terminal/galaxy_combat_board_terminal.iff")
