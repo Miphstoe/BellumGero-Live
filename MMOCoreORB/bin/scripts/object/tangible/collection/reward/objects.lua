71548de8eddf860c400aaf2d2b1a2c2c504eeafa
Miphstoe <nyre74@gmail.com>
2026-06-14 12:40:39 -0400

    Fixed Fish tank from not spawning on Live

--- MMOCoreORB/bin/scripts/object/tangible/collection/reward/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/collection/reward/objects.lua
@@ -1,6 +1,7 @@
 object_tangible_collection_reward_shared_fish_tank_reward = SharedTangibleObjectTemplate:new {
 	clientTemplateFileName = "object/tangible/collection/reward/shared_fish_tank_reward.iff",
-	gameObjectType = 8208
+	gameObjectType = 8211,
+	clientGameObjectType = 8211
 }
 
 ObjectTemplates:addClientTemplate(object_tangible_collection_reward_shared_fish_tank_reward, "object/tangible/collection/reward/shared_fish_tank_reward.iff")
