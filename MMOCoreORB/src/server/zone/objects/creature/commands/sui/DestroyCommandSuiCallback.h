1681f5fb5f5c11fdd7b672344eb861b7e8962f03
Frank Misemer III <misemerfg@gmail.com>
2026-09-09 08:36:12 -0500

    Revert "WIP checkpoint structure integrity Build 3.2A"
    This reverts commit ba6189f21da39c53f3b3eaa835e2aa02b3d80f73.

--- MMOCoreORB/src/server/zone/objects/creature/commands/sui/DestroyCommandSuiCallback.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/sui/DestroyCommandSuiCallback.h
@@ -9,8 +9,6 @@
 #define DESTROYCOMMANDSUICALLBACK_H_
 
 #include "server/zone/objects/player/sui/SuiCallback.h"
-#include "server/zone/objects/structure/StructureObject.h"
-#include "server/zone/managers/structure/StructureManager.h"
 
 class DestroyCommandSuiCallback : public SuiCallback {
 public:
@@ -34,27 +32,6 @@ public:
 			return;
 		}
 
-
-		// BELLUM_GERO_STRUCTURE_WORLD_REMOVAL_GUARD_BUILD32A_EXTERNAL_AUTH
-		// /destroy intentionally bypasses StructureManager::destroyStructure, so
-		// persistent buildings must explicitly authorize this one removal.
-		if (obj->isBuildingObject() && obj->getPersistenceLevel() > 0) {
-			ManagedReference<StructureObject*> structure = obj.castTo<StructureObject*>();
-			StructureManager* structureManager = StructureManager::instance();
-
-			if (structure != nullptr && structureManager != nullptr) {
-				Locker structureLocker(structure);
-				structureManager->authorizePersistentStructureWorldRemoval(structure);
-				structure->destroyObjectFromWorld(true);
-				structure->destroyObjectFromDatabase(true);
-
-				creature->sendSystemMessage("The object has been successfully destroyed from the database.");
-				return;
-			}
-
-			creature->sendSystemMessage("Persistent structure destruction was refused because the structure removal guard was unavailable.");
-			return;
-		}
 
 		obj->destroyObjectFromWorld(true);
 
